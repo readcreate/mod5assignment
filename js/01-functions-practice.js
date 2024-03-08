7d29fe5d7fc085f79934f69cc29cb19f7326cd10
at143 <anshulkumar@yahoo.com>
2024-03-07 19:22:40 -0500

    initial commit

---
+++ js/01-functions-practice.js
@@ -0,0 +1,12 @@
+//STEP 1
+// Write a function called halfNumber() that accepts one argument (a number), divide it by 2, and return the result. It should log a string like "Half of 5 is 2.5.".
+
+
+//STEP 2
+// Write a function called squareNumber() that accepts one argument (a number), square that number, and return the result. It should also log a string like "The result of squaring the number 3 is 9."
+
+//STEP 3
+// Write a function called percentOf() that accepts two numbers, figure out what percent the first number represents of the second number, and return the result. It should log a string like "2 is 50% of 4."
+
+//STEP 4
+// Write a function called findModulus() that accepts two numbers. Within the function write a statement that returns the modulus of the first and second parameters. It should log a string like "2 is the modulus of 4 and 10."
